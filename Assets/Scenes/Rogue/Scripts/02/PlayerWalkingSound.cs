9c4752e1b2c3462999ef67a897a655ec66dc97be
wiegandrobert <wiegand.robert@icloud.com>
2026-03-12 17:08:14 +0100

    different sound for room and corridor

--- Assets/Scenes/Rogue/Scripts/02/PlayerWalkingSound.cs
+++ Assets/Scenes/Rogue/Scripts/02/PlayerWalkingSound.cs
@@ -7,65 +7,125 @@ using System.ComponentModel;
 public class PlayerWalkingSound : MonoBehaviour
 {
 
-    public AudioClip walkSound;
-    public bool onMeHandle = true;
-    public bool onItHandle = false;
+    public AudioClip walkSoundRoom;
 
     [SerializeField]
     [Range(0f, 1f)]
     private float volume = 1f;
 
     [SerializeField]
-    [Range(0f, 20f)]
-    private float pitch = 1f;
+    [Range(500f, 22000f)]
+    private float corridorLowPassCutoff = 1800f;
 
     [SerializeField]
-    [Description("Sensitivity for detecting movement. Lower values make it more sensitive.")]
-    [Range(0f, 1f)]
-    private float movementDetection = 1f;
+    private AudioReverbPreset roomReverbPreset = AudioReverbPreset.Off;
 
-    private Vector3 lastPositionItHandle;
-    private Vector3 lastPositionMeHandle;
-
+    [SerializeField]
+    private AudioReverbPreset corridorReverbPreset = AudioReverbPreset.Cave;
     private AudioSource audioSource;
+    private AudioLowPassFilter lowPassFilter;
+    private AudioReverbFilter reverbFilter;
+
+    private readonly HashSet<Collider> roomTriggers = new HashSet<Collider>();
+    private readonly HashSet<Collider> corridorTriggers = new HashSet<Collider>();
+
+    private Vector3 lastPosition;
 
     void Awake()
     {
         this.audioSource = gameObject.AddComponent<AudioSource>();
+        this.lowPassFilter = gameObject.AddComponent<AudioLowPassFilter>();
+        this.reverbFilter = gameObject.AddComponent<AudioReverbFilter>();
+
         if (audioSource != null)
             audioSource.volume = volume;
-        audioSource.clip = walkSound;
+
+        if (lowPassFilter != null)
+            lowPassFilter.enabled = false;
+
+        if (reverbFilter != null)
+            reverbFilter.reverbPreset = roomReverbPreset;
+
+        audioSource.clip = walkSoundRoom;
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Room"))
+        {
+            roomTriggers.Add(other);
+            return;
+        }
+
+        if (other.CompareTag("Corridor"))
+        {
+            corridorTriggers.Add(other);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Room"))
+        {
+            roomTriggers.Remove(other);
+            return;
+        }
+
+        if (other.CompareTag("Corridor"))
+        {
+            corridorTriggers.Remove(other);
+        }
+    }
+
+    private bool IsCorridorOnly()
+    {
+        bool inRoom = roomTriggers.Count > 0;
+        bool inCorridor = corridorTriggers.Count > 0;
+
+        return inCorridor && !inRoom;
+    }
+
+    private void UpdateEnvironmentEffects()
+    {
+        bool useCorridorFx = IsCorridorOnly();
+
+        if (lowPassFilter != null)
+        {
+            lowPassFilter.enabled = useCorridorFx;
+            lowPassFilter.cutoffFrequency = corridorLowPassCutoff;
+        }
+
+        if (reverbFilter != null)
+        {
+            reverbFilter.reverbPreset = useCorridorFx ? corridorReverbPreset : roomReverbPreset;
+        }
+    }
+
     void Update()
     {
-        if (onMeHandle)
+        UpdateEnvironmentEffects();
+        
+        if(this.transform.position != lastPosition)
         {
-            Vector3 handlePos = GameObject.Find("Panto").GetComponent<UpperHandle>().GetPosition();
-            var handleDistance = UnityEngine.Vector3.Distance(handlePos, lastPositionMeHandle);
-
-            if (handleDistance > movementDetection)
-            {
-                lastPositionMeHandle = handlePos;
-                PlayWalkSound(handleDistance * pitch);
-            }
-        }
-        if (onItHandle)
-        {
-            Vector3 handlePos = GameObject.Find("Panto").GetComponent<LowerHandle>().GetPosition();
-            var handleDistance = UnityEngine.Vector3.Distance(handlePos, lastPositionItHandle);
-
-            if (handleDistance > movementDetection)
-            {
-                lastPositionItHandle = handlePos;
-                PlayWalkSound(handleDistance * pitch);
-            }
+            PlayWalkSound();
+            lastPosition = this.transform.position;
         }
     }
 
     async void PlayWalkSound(float speed = 1)
     {
+        if (walkSoundRoom == null || audioSource == null)
+        {
+            return;
+        }
+
+        if (audioSource.clip != walkSoundRoom)
+        {
+            audioSource.clip = walkSoundRoom;
+        }
+
         audioSource.pitch = speed;
-        if (audioSource != null && walkSound != null && !audioSource.isPlaying)
+        if (!audioSource.isPlaying)
         {
             audioSource.Play();
         }
