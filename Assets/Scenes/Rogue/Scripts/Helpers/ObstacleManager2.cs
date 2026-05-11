acd8c3f2c06561501c18f9cc066df7a28a77ae30
wiegandrobert <wiegand.robert@icloud.com>
2026-05-11 08:50:17 +0200

    ObstacleManager update (render walls during start)

---
+++ Assets/Scenes/Rogue/Scripts/Helpers/ObstacleManager2.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DualPantoToolkit;
+
+public class ObstacleManager2 : MonoBehaviour
+{
+    PantoCollider[] pantoColliders;
+    void Start()
+    {
+        Invoke("createObstacles", 2.0f);
+    }
+    
+
+    private void createObstacles()
+    {
+        pantoColliders = GameObject.FindObjectsOfType<PantoCollider>();
+        foreach (PantoCollider collider in pantoColliders)
+        {
+            collider.CreateObstacle();
+            collider.Enable();
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            foreach (PantoCollider collider in pantoColliders)
+            {
+                collider.Enable();
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            foreach (PantoCollider collider in pantoColliders)
+            {
+                collider.Disable();
+            }
+        }
+    }
+}
